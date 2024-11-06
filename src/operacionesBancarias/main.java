e842d485261816f6620e200617d9059c895294e4
CarlosAndrésLugoMesa <calugo04@misena.edu.co>
2024-11-06 09:49:20 -0500

    terminacion del proyectico

--- src/operacionesBancarias/main.java
+++ src/operacionesBancarias/main.java
@@ -15,45 +15,45 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Elige una opción: ");
-        System.out.println("0 - Abono a Cartera");
-        System.out.println("1 - Consignación");
-        System.out.println("2 - Transferencia");
-        System.out.println("3 - Pago de Nómina");
-        System.out.println("4 - Débito Automático");
-        System.out.println("5 - Depósito");
-        System.out.println("6 - Retiro");
+        System.out.println("1- Abono a Cartera");
+        System.out.println("2 - Consignación");
+        System.out.println("3 - Transferencia");
+        System.out.println("4 - Pago de Nómina");
+        System.out.println("5 - Débito Automático");
+        System.out.println("6 - Depósito");
+        System.out.println("7 - Retiro");
 
-        int tipo = scanner.nextInt(); // Leer la opción del usuario
+        int tipo = scanner.nextInt();
         Cuentas cuenta = null;
         Procesos procesos = null;
 
-        if (tipo == 0) {
+        if (tipo == 1) {
             cuenta = new Creditos();
             procesos = new AbonarCartera();
             ((AbonarCartera) procesos).procesarCredito((Creditos) cuenta);
-        } else if (tipo == 1) {
+        } else if (tipo == 2) {
             cuenta = new CuentaNomina();
             procesos = new Consignacion();
             ((Consignacion) procesos).procesarConsginacion((CuentaNomina) cuenta);
-        } else if (tipo == 2) {
+        } else if (tipo == 3) {
             cuenta = new CuentasRepresentante();
             CuentaExtranjero cuentaExtranjero = new CuentaExtranjero();
             procesos = new Transferencias();
             ((Transferencias) procesos).hacerTranferencia((CuentasRepresentante) cuenta, cuentaExtranjero);
-        } else if (tipo == 3) {
+        } else if (tipo == 4) {
             cuenta = new CuentaNomina();
             procesos = new PagoNomina();
             ((PagoNomina) procesos).realizarPago(cuenta);
-        } else if (tipo == 4) {
+        } else if (tipo == 5) {
             cuenta = new CuentaNomina();
             procesos = new DebitoAutomatico();
             ((DebitoAutomatico) procesos).realizarDebito(cuenta, 20);
-        } else if (tipo == 5) {
+        } else if (tipo == 6) {
             Deposito deposito = new Deposito();
             deposito.setSaldo(100);
             deposito.realizarDeposito(50);
             cuenta = new CuentaNomina();
-        } else if (tipo == 6) {
+        } else if (tipo == 7) {
             Retiro retiro = new Retiro();
             retiro.setSaldo(100);
             retiro.getSaldo();
@@ -61,18 +61,14 @@ public class main {
             cuenta = new CuentaNomina();
         }
 
-
         if (cuenta != null) {
             System.out.println(cuenta.consultarSaldo());
         }
 
-
-
-            for (int h = 0; h < procesos.listarMovimientos().size(); h++) {
+        for (int h = 0; h < procesos.listarMovimientos().size(); h++) {
                 System.out.println("Número de cuenta: " + procesos.listarMovimientos().get(h).getNumeroCuenta());
 
-
-            }
+        }
 
 
     }
